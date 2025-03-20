bcd5d84c688311187466c56fc3ef04be22734739
Jonah Coffelt <coffelt.jonah0918@gmail.com>
2025-03-20 15:25:52 -0500

    formatting

--- main.py
+++ main.py
@@ -14,14 +14,13 @@ mud_albedo   = bsk.Image('textures/mud.png')
 mud_normal   = bsk.Image('textures/mud_normal.png')
 cloth_albedo = bsk.Image('textures/cloth_albedo.png')
 cloth_normal = bsk.Image('textures/cloth_normal.png')
-
 landscape = bsk.Image('textures/landscape.png')
 
 # Load materials
 floor     = bsk.Material(texture=floor_albedo, normal=floor_normal, roughness=.25, specular=2, clearcoat=1, anisotropic=.25)
 mud       = bsk.Material(texture=mud_albedo, normal=mud_normal, roughness=.5, specular=2, clearcoat=.5)
 cloth     = bsk.Material(texture=cloth_albedo, normal=cloth_normal, roughness=.8, specular=1.5, clearcoat=.8, clearcoat_gloss=.6)
-emissive  = bsk.Material()
+emissive  = bsk.Material(emissive_color=(500, 150, 150))
 landscape = bsk.Material(texture=landscape)
 
 # Add Nodes
